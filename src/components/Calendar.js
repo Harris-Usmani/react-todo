2ee941ca3dc983a7067821012a98c077f46ddbdc
Harris-Usmani <harisusmani964@gmail.com>
2021-11-01 13:34:39 +0500

    Added calendar/nav icons

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,13 +1,41 @@
 import React from 'react'
+import { CalendarDate, CaretUp } from 'react-bootstrap-icons';
 
 function Calendar() {
+    
+    //Constant menu items
+    const calendarItems = ['today', 'next 7 days', 'all days']
 
     return (
 
         <div className="Calendar">
 
-            Calendar
+            <div className='header'>
+                <div className='title'>
+                    <CalendarDate size='18' />
+                    <p>Calendar</p>
+                </div>
 
+                <div className='btns'>
+                    <span>
+                        <CaretUp size='20' />
+                    </span>
+                </div>
+            </div>
+
+            <div className='items'> 
+
+                {
+                    calendarItems.map(item => 
+                        <div className='item' key={item}>
+                            {item}
+                        </div>
+                        
+                        ) 
+                }
+
+            </div>
+                
         </div>
 
     )
